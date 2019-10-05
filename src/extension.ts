3819e24d37a3f2ff0652b45fecdb912be44f8d11
Christoph Schlosser <christoph@linux.com>
2019-10-05 15:43:55 +0200

    Prepare release 0.5.1

--- src/extension.ts
+++ src/extension.ts
@@ -12,8 +12,8 @@ enum AlignmentNotificationOptions {
 }
 
 enum Version {
-    CURRENT = "0.5.0",
-    PREVIOUS = "0.4.3",
+    CURRENT = "0.5.1",
+    PREVIOUS = "0.5.0",
     KEY = "doxdocgen_version",
 }
 
@@ -24,35 +24,10 @@ export function activate(context: vscode.ExtensionContext) {
 
     context.subscriptions.push(parser);
 
-    let change: boolean = false;
     const version = context.globalState.get<string>(Version.KEY);
     if (version === undefined) {
         context.globalState.update(Version.KEY, Version.CURRENT);
     } else if (version !== Version.CURRENT) {
-        if (parseInt(version.split(".")[1], 10) < 5) {
-            change = true;
-        }
         context.globalState.update(Version.KEY, Version.CURRENT);
     }
-    let notificationHideThenable: Thenable<string>;
-    const active = context.globalState.get<string>(AlignmentNotificationOptions.GLOBAL_STORAGE_KEY);
-    if (change && (active === undefined || active !== "false")) {
-        // tslint:disable-next-line:max-line-length
-        notificationHideThenable =
-            vscode.window.showWarningMessage("DoxDocGen: I can now align your comments",
-                                             AlignmentNotificationOptions.CHANGED,
-                                             AlignmentNotificationOptions.HIDE);
-    }
-
-    if (notificationHideThenable !== undefined) {
-        notificationHideThenable.then((action) => {
-            if (action === AlignmentNotificationOptions.CHANGED) {
-                // tslint:disable-next-line:max-line-length
-                opn("https://github.com/christophschlosser/doxdocgen/blob/master/CHANGELOG.md#alignment");
-            } else if (action === AlignmentNotificationOptions.HIDE) {
-                context.globalState.update(AlignmentNotificationOptions.GLOBAL_STORAGE_KEY, "false");
-            }
-        });
-    }
-
 }
