6def52f47e32887ab825cdf8e1d7669c0f782be8
Christoph Schlosser <christoph@linux.com>
2020-06-29 21:14:29 +0200

    Release 0.7.1

--- src/extension.ts
+++ src/extension.ts
@@ -5,8 +5,8 @@ import * as vscode from "vscode";
 import CodeParserController from "./CodeParserController";
 
 enum Version {
-    CURRENT = "0.7.0",
-    PREVIOUS = "0.6.0",
+    CURRENT = "0.7.1",
+    PREVIOUS = "0.7.0",
     KEY = "doxdocgen_version",
 }
 
