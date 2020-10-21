e4422a95ef865a41b16b083034d02e926a3b8fcd
Christoph Schlosser <christoph@linux.com>
2020-10-21 17:52:11 +0200

    Add coverage

--- src/test/runTests.ts
+++ src/test/runTests.ts
@@ -1,8 +1,28 @@
 import * as path from "path";
-
 import { runTests } from "vscode-test";
 
+function setupCoverage() {
+  const NYC = require("nyc");
+  const nyc = new NYC({
+    all: true,
+    cwd: path.join(__dirname, "..", "..", ".."),
+    exclude: ["src/test/**", ".vscode-test/**", "**/node_modules/**"],
+    hookRequire: true,
+    hookRunInContext: true,
+    hookRunInThisContext: true,
+    instrument: true,
+    reportDir: path.join(__dirname, "..", "..", "coverage"),
+    reporter: ["json", "html", "lcov"],
+  });
+
+  nyc.reset();
+  nyc.wrap();
+
+  return nyc;
+}
+
 async function main() {
+  const nyc = process.env.COVERAGE ? setupCoverage() : null;
   try {
     // The folder containing the Extension Manifest package.json
     // Passed to `--extensionDevelopmentPath`
@@ -10,7 +30,7 @@ async function main() {
 
     // The path to the extension test runner script
     // Passed to --extensionTestsPath
-    const extensionTestsPath = path.resolve(__dirname, "./index");
+    const extensionTestsPath = path.resolve(__dirname, "./coverage");
 
     // Download VS Code, unzip it and run the integration test
     await runTests({ extensionDevelopmentPath, extensionTestsPath });
@@ -20,6 +40,11 @@ async function main() {
     // tslint:disable-next-line: no-console
     console.error("Failed to run tests");
     process.exit(1);
+  } finally {
+    if (nyc) {
+      nyc.writeCoverageFile();
+      await nyc.report();
+    }
   }
 }
 
