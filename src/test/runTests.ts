551d3e4876f51d9151c1eec110901e681b527468
Christoph Schlosser <christoph@linux.com>
2021-04-15 21:50:52 +0200

    copy&paste mistake

--- src/test/runTests.ts
+++ src/test/runTests.ts
@@ -10,7 +10,7 @@ async function main() {
 
     // The path to the extension test runner script
     // Passed to --extensionTestsPath
-    const extensionTestsPath = path.resolve(__dirname, "./suite/index");
+    const extensionTestsPath = path.resolve(__dirname, "./index");
 
     // Download VS Code, unzip it and run the integration test
     await runTests({ extensionDevelopmentPath, extensionTestsPath });
